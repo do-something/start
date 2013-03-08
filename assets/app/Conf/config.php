7fede3896908169c6e4c7360fc537954ade98292
taballa.hp-PD <taballa@gmail.com>
2013-03-08 10:18:30 +0800

    update;

--- assets/app/Conf/config.php
+++ assets/app/Conf/config.php
@@ -4,7 +4,8 @@ return array(
     //'配置项'=>'配置值'
     'SHOW_PAGE_TRACE'=>true,
     'URL_HTML_SUFFIX'=>'.html',
-    'URL_MODEL'      => 2       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    'URL_MODEL'      => 2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式，提供最好的用户体验和SEO支持
+    'URL_CASE_INSENSITIVE' =>true
 );
 ?>
---
+++ assets/app/index_dist.php
@@ -0,0 +1,15 @@
+<?php 
+define('APP_DEBUG', true); // 开启调试模式
+// define('APP_NAME','app');
+define('APP_NAME','dist');
+// define('APP_PATH','./');
+define('APP_PATH','../dist/');
+
+define('EXTEND_PATH', '../../thinkphp/thinkphp-extend/Extend/');
+define('ENGINE_NAME', 'sae');
+define('ENGINE_PATH', '../../thinkphp/thinkphp-extend/Extend/Engine/');
+define('RUNTIME_PATH',APP_PATH.'Runtime/');
+define('THINK_PATH', '../../thinkphp/ThinkPHP/');
+
+require('../../thinkphp/ThinkPHP/ThinkPHP.php');
+// require('../../thinkphp/thinkphp-extend/Extend/Engine/sae.php');
