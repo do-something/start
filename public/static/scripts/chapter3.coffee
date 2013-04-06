a0b603fefeed2b62eaad24566c2b1fc722caac2e
taballa-hp-mini <taballa@gmail.com>
2013-04-06 16:15:24 +0800

    update

--- public/static/scripts/chapter3.coffee
+++ public/static/scripts/chapter3.coffee
@@ -3,4 +3,5 @@ define ['jquery', 'bootstrap'], ($)->
     $('h6').css('font-size', '22px')
     $('#hide-sidebar').click ->
         $('#sidebar').toggle()
-
+    $('#html').html('$("#html").html(<a href="#">a</a>)')
+    $('#text').text('$("#text").text(<a href="#">a</a>)')
--- public/static/scripts/chapter3.js
+++ public/static/scripts/chapter3.js
@@ -2,9 +2,11 @@
 
   define(['jquery', 'bootstrap'], function($) {
     $('h6').css('font-size', '22px');
-    return $('#hide-sidebar').click(function() {
+    $('#hide-sidebar').click(function() {
       return $('#sidebar').toggle();
     });
+    $('#html').html('$("#html").html(<a href="#">a</a>)');
+    return $('#text').text('$("#text").text(<a href="#">a</a>)');
   });
 
 }).call(this);
