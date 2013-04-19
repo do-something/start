ad2215a710b31c3299800ae8b20775cb1587dc85
taballa.hp-PD <taballa@gmail.com>
2013-04-19 20:31:52 +0800

    add TMPL_PARSE_STRING but is Error

--- app/Conf/config.php
+++ app/Conf/config.php
@@ -2,14 +2,17 @@
 //注意，请不要在这里配置SAE的数据库，配置你本地的数据库就可以了。
 return array(
     //'配置项'                             => '配置值'
-    'SHOW_PAGE_TRACE'                   => false,
+    // 'SHOW_PAGE_TRACE'                   => ture,
     'URL_HTML_SUFFIX'                   => '.html',
     // URL访问模式可选参数0、1、2、3代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式，提供最好的用户体验和SEO支持
     'URL_MODEL'                         => 2,
     'URL_CASE_INSENSITIVE'              => true,  // URL大小写敏感
-    'LAYOUT_ON'                         => true,
+    'LAYOUT_ON'                         => false,
     'URL_HTML_SUFFIX'                   => false,  // 伪静态后缀
+    'TMPL_PARSE_STRING'                 => array(
+        // '__UPLOAD__'                        => __ROOT__.'/upload'
+    ),
 
     // 路由 ROUTER
     'URL_ROUTER_ON'                     => true,
